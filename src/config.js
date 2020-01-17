0fb83ba2961ff8d87d99f41f6c7bfcc15268acbc
Pasha Tay <pashabonilla@gmail.com>
2020-01-16 19:26:57 -0500

    .

--- src/config.js
+++ src/config.js
@@ -1,7 +1,11 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: process.env.DB_URL || "postgresql://postgres@localhost/dreamlog",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://postgres@localhost/dreamlog",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    "postgresql://postgres@localhost/dreamlog-test",
   JWT_KEY: process.env.JWT_KEY || "Newyork",
   EMAIL_PASSWORD: process.env.EMAIL_PASSWORD || "76Jy8.9Ioo"
 };
